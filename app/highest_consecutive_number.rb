33b0755d24ac7d690e23fe8183c3a7f7e2a3bc75
Bryan Finlayson <bdfinlayson@gmail.com>
2015-07-06 22:27:38 -0500

    Add solution to code challenge

--- app/highest_consecutive_number.rb
+++ app/highest_consecutive_number.rb
@@ -1,5 +1,48 @@
+require 'pry'
+
 class HighestNumber
-  def hello
-    "hello"
+  # iterate through input
+  # add each item to create a new sum
+  # compare new sum with previous sum
+  # check if is at end of array
+  # if new sum is larger, continues to next item
+  # if it is smaller, return
+  # continue to next positive and start the process over till end of array is reached
+
+  def initialize
+    @prev_sum = 0
+    @results = Array.new
+  end
+
+  def add_nums(input)
+    input.each do |num|
+      if new_sum_is_greater?(num)
+        @prev_sum += num
+        @results << @prev_sum if input.size - 1 == input.index(num)
+      else
+        @results << @prev_sum
+        @prev_sum = 0
+      end
+    end
+    parse_results
+  end
+
+  private
+
+  def parse_results
+    array = Array.new
+    @results.each do |item|
+      array << item if item.kind_of?(Fixnum)
+    end
+    array.max
+  end
+
+  def new_sum_is_greater?(num, new_sum = 0)
+    new_sum = @prev_sum + num if num.kind_of?(Fixnum)
+    if new_sum > @prev_sum
+      true
+    else
+      false
+    end
   end
 end
--- spec/highest_consecutive_number_spec.rb
+++ spec/highest_consecutive_number_spec.rb
@@ -3,4 +3,27 @@ require 'spec_helper'
 describe HighestNumber do
   let(:hn) { HighestNumber.new }
 
+  it 'should return the largest sum of consecutive postitive numbers' do
+    array = [1,2,3,4,5,6]
+    result = hn.add_nums(array)
+    expect(result).to eq 21
+  end
+
+  it 'should return the largest sum of a mixed array' do
+    array = [1,2,3,4,5,-2,1,2,3,4]
+    result = hn.add_nums(array)
+    expect(result).to eq 15
+  end
+
+  it 'should return the largest with many negatives' do
+    array = [1,2,-5,-2,1,4,-5,-3,8,9,0,-2,-7,10]
+    result = hn.add_nums(array)
+    expect(result).to eq 17
+  end
+
+  it 'should reject non-number characters' do
+    array = [1,2,'a','$','%',5,6,'xyz',10]
+    result = hn.add_nums(array)
+    expect(result).to eq 11
+  end
 end
